20409a5b2e79c1a80e4ab7ad6dc24d331690c9e9
ruanifsilva <ruanifsilva@gmail.com>
2021-12-01 22:53:51 -0300

    chore:adding django

---
+++ backend/ponto_certo/config/urls.py
@@ -0,0 +1,26 @@
+"""ponto_certo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.core.urls', namespace='core')),
+    path('cartoes/', include('apps.cartoes.urls', namespace='cartoes')),
+    path('empresas/', include('apps.empresas.urls', namespace='empresas')),
+    path('funcionarios/', include('apps.funcionarios.urls', namespace='funcionarios')),
+    path('registros/', include('apps.registros.urls', namespace='registros')),
+]
