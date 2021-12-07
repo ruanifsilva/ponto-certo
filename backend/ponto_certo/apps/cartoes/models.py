a209438612df21904411ead36db8cb30b6857fb8
ruanifsilva <ruanifsilva@gmail.com>
2021-12-07 09:40:06 -0300

    refactor: changing models

--- backend/ponto_certo/apps/cartoes/models.py
+++ backend/ponto_certo/apps/cartoes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from backend.ponto_certo.apps.registros.models import RegistoHorario
+from backend.ponto_certo.apps.funcionarios.models import Funcionarios
 
 
 class CartaoPontoModelo(models.Model):
+    funcionario = models.ForeignKey(Funcionarios, on_delete=models.CASCADE)
     cartao_registrado = models.ForeignKey(RegistoHorario, on_delete=models.CASCADE)
     hora_entrada = models.DateTimeField()
     hora_saida_almoco = models.DateTimeField()
--- backend/ponto_certo/apps/funcionarios/models.py
+++ backend/ponto_certo/apps/funcionarios/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from backend.ponto_certo.apps.empresas.models import Empresa
-from backend.ponto_certo.apps.cartoes.models import CartaoPontoModelo
+
 
 
 class Funcionarios(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    cartao_ponto_modelo = models.ForeignKey(CartaoPontoModelo, on_delete=models.CASCADE)
     nome = models.CharField(max_length=200)
     cpf = models.IntegerField(max_length=11, blank=False)
     funcao = models.CharField(max_length=200)
